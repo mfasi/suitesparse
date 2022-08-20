abea71b20ae0521cd54a58f919aa6d4ddc8de181
Massimiliano Fasi <massimiliano.fasi@durham.ac.uk>
2022-08-20 20:26:57 +0100

    Generate skeleton of generator

--- setup.m
+++ setup.m
@@ -1,9 +1,10 @@
 function setup()
   suitesparse_url = 'https://sparse.tamu.edu/';
+  suitesparse_files_url = [suitsparse_url '/files/'];
   index_file_name = 'ss_index.mat';
 
   % Download and load index.
-  index_file_url = [suitesparse_url '/files/' index_file_name];
+  index_file_url = [suitesparse_files_url index_file_name];
   websave(index_file_name, index_file_url);
   tmp = load(index_file_name); % Contains struct ss_index.
   ss_index = tmp.ss_index;
@@ -13,15 +14,15 @@ function setup()
   if exist(timestamp_filename, 'file')
     % If timestamp file already present, check if update is needed.
     timestamp_file = fopen(timestamp_filename, 'r');
-     [old_timestamp, line_terminator] = fgets(timestamp_file);
-     fclose(timestamp_file);
-     if strcmp(ss_index.LastRevisionDate, old_timestamp)
-       return; % No need to update, index file has not changed.
-     end
-   end
+    [old_timestamp, line_terminator] = fgets(timestamp_file);
+    fclose(timestamp_file);
+    if strcmp(ss_index.LastRevisionDate, old_timestamp)
+      return; % No need to update, index file has not changed.
+    end
+  end
 
-   % If an update is needed or this is the first time the collection is
-   % generated, then update the timestamp.
+  % If an update is needed or this is the first time the collection is
+  % generated, then update the timestamp.
    timestamp_file = fopen(timestamp_filename, 'w');
    % fprintf(timestamp_file, ss_index.LastRevisionDate);
    fclose(timestamp_file);
@@ -40,9 +41,11 @@ function setup()
      end
 
      % Create a new generator for the current matrix in the group directory.
+     create_matrix_generator(group_dir, matrix_ID);
    end
 
-   function create_group_dir(group_ID, group_dir)
+
+   function create_group_dir(matrix_ID, group_dir)
 
    % Create directory.
      mkdir(group_dir);
@@ -57,7 +60,27 @@ function setup()
                            '= handle(varargin{1:nargin-1});\n end']);
      fclose(bridge_file);
 
-     % Create private directory.
+     % Create 'private' and 'private/matfiles' directories.
      mkdir([group_dir filesep 'private']);
+     mkdir([group_dir filesep 'private' filesep 'matfiles']);
+
    end
+
+   function create_matrix_generator(matrix_ID, group_dir)
+     generator_filename = [group_dir filesep matrix_ID];
+     generator_file = fopen(generator_filename);
+     group_private_dir = [group_dir filesep 'private'];
+     group_matfiles_dir = [group_private_dir filesep 'matfiles']
+     fprintf(generator_file, ['[A, properties] = function ' matrix_ID '()\n'...
+                              '  matfile_name = ['...
+                              group_matfiles_dir filesep matrix_ID...
+                              '''.mat''];\n'...
+                              '  if ~exist(matfile_name, ''dir'')\n'...
+                              '     websave(matfile_name,'...
+                              suitesparse_files_url '/'  matrix_ID...
+                              '''.mat'');\n'...
+                              '  end\n'
+                              'end'];
+             fclose(generator_file);
+             end
 end
