fd943b97c1ec407667e717fbdffd0382a8aad71d
Massimiliano Fasi <massimiliano.fasi@durham.ac.uk>
2022-08-22 09:29:59 +0100

    Rework structure for passing data to sub-functions

--- setup.m
+++ setup.m
@@ -1,19 +1,22 @@
 function setup()
-  suitesparse_url = 'https://sparse.tamu.edu/';
-  suitesparse_files_url = [suitsparse_url '/files/'];
-  index_file_name = 'ss_index.mat';
+  pkg.ss_url = 'https://sparse.tamu.edu/';
+  pkg.anymatrix_root_dir = fileparts(which('anymatrix'));
+  pkg.ss_private_root_dir = fileparts(mfilename('fullpath'));
 
   % Download and load index.
-  index_file_url = [suitesparse_files_url index_file_name];
-  websave(index_file_name, index_file_url);
-  tmp = load(index_file_name); % Contains struct ss_index.
+  index_filename = 'ss_index.mat';
+  index_file_url = [pkg.ss_url '/files/' index_filename];
+  index_file_path = [pkg.ss_private_root_dir filesep index_filename];
+  websave(index_file_path, index_file_url);
+  tmp = load(index_filename); % Contains struct ss_index.
   ss_index = tmp.ss_index;
 
   % Check if update is needed using a file that contains a timestamp.
   timestamp_filename = './timestamp';
-  if exist(timestamp_filename, 'file')
+  timestamp_file_abs_path = [pkg.ss_private_root_dir filesep timestamp_filename];
+  if exist(timestamp_file_abs_path, 'file')
     % If timestamp file already present, check if update is needed.
-    timestamp_file = fopen(timestamp_filename, 'r');
+    timestamp_file = fopen(timestamp_file_abs_path, 'r');
     [old_timestamp, line_terminator] = fgets(timestamp_file);
     fclose(timestamp_file);
     if strcmp(ss_index.LastRevisionDate, old_timestamp)
@@ -23,64 +26,83 @@ function setup()
 
   % If an update is needed or this is the first time the collection is
   % generated, then update the timestamp.
-   timestamp_file = fopen(timestamp_filename, 'w');
+   timestamp_file = fopen(timestamp_file_abs_path, 'w');
    % fprintf(timestamp_file, ss_index.LastRevisionDate);
    fclose(timestamp_file);
 
    % Generate the matrix generators in ephemeral groups.
-   anymatrix_root_dir = fileparts(which('anymatrix'));
    n_matrices = length(ss_index.Group);
-   for i = 1:n_matrices
-     group_ID = ss_index.Group{i};
-     matrix_ID = ss_index.Name{i};
+   for i = 1:10 % n_matrices
+     curr_mat.group_ID = lower(ss_index.Group{i});
+     curr_mat.matrix_ID = lower(ss_index.Name{i});
 
      % Create a new directory for the group of the current matrix, if necessary.
-     group_dir = [anymatrix_root_dir filesep group_ID];
-     if ~exist(group_dir, 'dir')
-       create_group_dir(group_ID, group_dir)
+     curr_mat.group_dir = [pkg.anymatrix_root_dir filesep curr_mat.group_ID];
+     curr_mat.group_private_dir = [curr_mat.group_dir filesep 'private'];
+     curr_mat.group_matfiles_dir = [curr_mat.group_private_dir filesep 'matfiles'];
+     if ~exist(curr_mat.group_dir, 'dir')
+       create_group_dir(curr_mat)
+       % else % Killer feature
+       %   rmdir(group_dir, 's');
      end
 
      % Create a new generator for the current matrix in the group directory.
-     create_matrix_generator(group_dir, matrix_ID);
+     create_matrix_generator(curr_mat);
    end
 
 
-   function create_group_dir(matrix_ID, group_dir)
-
+   function create_group_dir(curr_mat)
    % Create directory.
-     mkdir(group_dir);
+     mkdir(curr_mat.group_dir);
 
      % Create bridge function.
-     bridge_filename = [group_dir filesep 'anymatrix_' group_ID '.m'];
+     bridge_filename = [curr_mat.group_dir filesep...
+                        'anymatrix_' curr_mat.group_ID '.m'];
      bridge_file = fopen(bridge_filename, 'w');
-     fprintf(bridge_file, ['function varargout = anymatrix_' group_ID...
+     fprintf(bridge_file, ['function varargout = anymatrix_' curr_mat.group_ID...
                            '(matrix_name, varargin)\n'...
-                           'handle = str2func(matrix_name);\n'...
+                           'handle = str2func([''ss_'' matrix_name]);\n'...
                            '[varargout{1:nargout}]'...
                            '= handle(varargin{1:nargin-1});\n end']);
      fclose(bridge_file);
 
      % Create 'private' and 'private/matfiles' directories.
-     mkdir([group_dir filesep 'private']);
-     mkdir([group_dir filesep 'private' filesep 'matfiles']);
+     % mkdir([group_dir filesep 'private']);
+     mkdir(curr_mat.group_matfiles_dir);
+
+     % Create parser function.
+     parser_filename = [curr_mat.group_private_dir filesep...
+                        'anymatrix_parser_' curr_mat.group_ID '.m'];
+     parser_file = fopen(parser_filename, 'w');
+     fprintf(parser_file, ['function parsed_name = '...
+                           'anymatrix_parser_' curr_mat.group_ID...
+                           '(matrix_name)\n'...
+                           '  parsed_name = '...
+                           'extractAfter(matrix_name, ''ss_'');\n'...
+                           'end']);
+     fclose(parser_file);
 
    end
 
-   function create_matrix_generator(matrix_ID, group_dir)
-     generator_filename = [group_dir filesep matrix_ID];
-     generator_file = fopen(generator_filename);
-     group_private_dir = [group_dir filesep 'private'];
-     group_matfiles_dir = [group_private_dir filesep 'matfiles']
-     fprintf(generator_file, ['[A, properties] = function ' matrix_ID '()\n'...
-                              '  matfile_name = ['...
-                              group_matfiles_dir filesep matrix_ID...
-                              '''.mat''];\n'...
-                              '  if ~exist(matfile_name, ''dir'')\n'...
-                              '     websave(matfile_name,'...
-                              suitesparse_files_url '/'  matrix_ID...
-                              '''.mat'');\n'...
-                              '  end\n'
-                              'end'];
-             fclose(generator_file);
-             end
+   function create_matrix_generator(curr_mat)
+     function_name = ['ss_' curr_mat.matrix_ID];
+     generator_filename = [curr_mat.group_private_dir filesep...
+                           function_name '.m'];
+     generator_file = fopen(generator_filename, 'w');
+     fprintf(generator_file, ['function [A, properties] = ' function_name '()\n'...
+                              '  matfile_name = '''...
+                              curr_mat.group_matfiles_dir filesep...
+                              curr_mat.matrix_ID '.mat'';\n'...
+                              '  if ~exist(matfile_name, ''file'')\n'...
+                              '     matfile_url = '''...
+                              pkg.ss_url '/mat/' curr_mat.group_ID...
+                              '/' curr_mat.matrix_ID '.mat'';\n'...
+                              '    websave(matfile_name, matfile_url);\n'...
+                              '  end\n'...
+                              '  tmp = load(matfile_name, ''Problem'');\n'...
+                              '  A = tmp.Problem.A;\n'...
+                              '  properties' '= {};\n'...
+                              'end']);
+     fclose(generator_file);
+   end
 end
